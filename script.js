0a93c95308fadf7f53ed5f4d236a7a9a05c6f289
Zanzong43 <alexkoszuta43@gmail.com>
2021-03-30 13:39:05 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var count1 = 1;
+var count2 = 1;
+var count3 = 1;
+
+
+function add1() {
+    var count1Element = document.querySelector("#c1");
+    console.log("click");
+    console.log(count1Element);
+    count1++;
+    count1Element.innerText = count1 + " like(s)";
+}
+
+function add2() {
+    var count1Element = document.querySelector("#c2");
+    console.log("click");
+    console.log(count1Element);
+    count2++;
+    count1Element.innerText = count2 + " like(s)";
+}
+
+function add3() {
+    var count1Element = document.querySelector("#c3");
+    console.log("click");
+    console.log(count1Element);
+    count3++;
+    count1Element.innerText = count3 + " like(s)";
+}
